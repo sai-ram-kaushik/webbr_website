585eea739ea36ada90f9cfe143c1950450655ccf
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-02-26 23:07:43 +0530

    updated with the layout

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <div className="w-full h-16 px-5 lg:px-10">
+      <div className="container mx-auto w-full h-full">
+        <div className="flex items-center justify-between w-full h-full">
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "#FEFEFE",
+        primary: "#000000",
+        secondary: "#F57A1C",
       },
+
+      fontFamily: {
+        heading: "Montserrat",
+        content: "PT Sans"
+      }
     },
   },
   plugins: [],
